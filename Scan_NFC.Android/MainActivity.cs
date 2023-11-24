93064e84807179ef0ac855323ad547cad0715be6
EduardoEliasCMAK <eduardo.silva@cmak.ind.br>
2023-11-24 16:10:25 -0300

    Adicionar arquivos de projeto.

---
+++ Scan_NFC.Android/MainActivity.cs
@@ -0,0 +1,36 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.OS;
+using Plugin.NFC;
+
+namespace Scan_NFC.Droid
+{
+    [Activity(Label = "Scan_NFC", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            CrossNFC.Init(this);
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+        }
+        protected override void OnResume()
+        {
+            base.OnResume();
+            CrossNFC.OnResume();
+        }
+        protected override void OnNewIntent(Android.Content.Intent intent)
+        {
+            base.OnNewIntent(intent);
+            CrossNFC.OnNewIntent(intent);
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ Scan_NFC/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using Plugin.NFC;
+using System;
+using Xamarin.Forms;
+
+namespace Scan_NFC
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // Atribui o Entry com o nome 'meuEntry' à variável no código-behind
+            meuEntry = (Entry)FindByName("meuEntry");
+
+            // Inicializa o NFC e registra o evento OnMessageReceived
+            CrossNFC.Current.OnMessageReceived += NfcMessageReceived;
+        }
+
+        private void NfcMessageReceived(ITagInfo tagInfo)
+        {
+            var idBytes = tagInfo.Identifier;
+            var message = BitConverter.ToString(idBytes);
+
+            // Atualiza o Entry com o valor da tag NFC
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                meuEntry.Text = $"Tag NFC Lida - ID: {message}";
+                // Exibe um alerta opcional
+                DisplayAlert("Tag NFC Lida", $"ID: {message}", "OK");
+            });
+        }
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            System.Diagnostics.Debug.WriteLine("MainPage OnAppearing");
+
+            CrossNFC.Current.StartListening();
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            System.Diagnostics.Debug.WriteLine("MainPage OnDisappearing");
+
+            CrossNFC.Current.StopListening();
+        }
+    }
+}
